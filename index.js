3abb3b0baef397e0c5f0521ccfeb7ddeb103803b
inaraRibeiro <39559304+inaraRibeiro@users.noreply.github.com>
2022-05-07 11:21:40 -0300

    Add files via upload
    novas alterações.

--- index.js
+++ index.js
@@ -1,10 +1,32 @@
-function smi(mT, fC, fA, fD) {
+function smi() {
+    let buttons = document.querySelectorAll('div.itens > button');
+
+    buttons.forEach((item, index) => {
+        item.onclick = function () {
+            if (index == 0) {
+                let mT = prompt('Informe o valor do MT: ');
+            }
+            if (index == 1) {
+                let fC = prompt('Informe o valor do MT: ');
+            }
+            if (index == 2) {
+                let fA = prompt('Informe o valor do MT: ');
+            }
+            if (index == 3) {
+                let fD = prompt('Informe o valor do MT: ');
+            }
+
+        }
+    });
+
     let smi
+
     smi = ((mT - (fA + fC + fD)) / mt)
     return parseFloat(smi)
 }
 
 function integridade(seg, ameaca) {
+
     let integridade
     integridade = (1 - (ameaca * (1 - seg)))
     return parseFloat(integridade)
@@ -12,7 +34,7 @@ function integridade(seg, ameaca) {
 
 function volume1(n1, n2, NUM2) {
     let vlProg
-    vlProg = ((NUM2*Math.log(2)) * (n1 + n2))
+    vlProg = ((NUM2 * Math.log(2)) * (n1 + n2))
 
 
     return parseFloat(vlProg)
