525a029a1e48137de69069216b67942b14d87aaa
inaraRibeiro <39559304+inaraRibeiro@users.noreply.github.com>
2022-05-07 11:02:15 -0300

    Add files via upload
    criadas funções para calculo das métricas.

---
+++ index.js
@@ -0,0 +1,24 @@
+function smi(mT, fC, fA, fD) {
+    let smi
+    smi = ((mT - (fA + fC + fD)) / mt)
+    return parseFloat(smi)
+}
+
+function integridade(seg, ameaca) {
+    let integridade
+    integridade = (1 - (ameaca * (1 - seg)))
+    return parseFloat(integridade)
+}
+
+function volume1(n1, n2, NUM2) {
+    let vlProg
+    vlProg = ((NUM2*Math.log(2)) * (n1 + n2))
+
+
+    return parseFloat(vlProg)
+}
+
+
+
+
+
